ac2bb3dbf733700ea3c9baf73940e1d43940056a
archanfel2016 <hahahehe95@163.com>
2022-09-03 15:10:50 +0800

    更新 filehelper.py

--- filehelper.py
+++ filehelper.py
@@ -4,4 +4,5 @@ def get_files(directoryName,filter="*.*",recu=True):
     wp=WindowsPath(directoryName)
     func=wp.rglob if recu else wp.glob
     return [str(file) for file in func(filter)]
+    
 
