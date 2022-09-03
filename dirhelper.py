9eea4bf809f654c258b053667661c3f6619a1515
archanfel2016 <hahahehe95@163.com>
2022-09-03 15:10:50 +0800

    更新 filehelper.py

--- dirhelper.py
+++ dirhelper.py
@@ -3,5 +3,5 @@ from pathlib import WindowsPath
 def get_files(directoryName,filter="*.*",recu=True):
     wp=WindowsPath(directoryName)
     func=wp.rglob if recu else wp.glob
-    return [str(file) for file in func(filter)]
+    return [str(file) for file in func(filter)]  
 
